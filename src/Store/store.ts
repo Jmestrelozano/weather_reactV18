a0aabffcc2d45dbb6b1e4aaddb9915171737908e
Jmestrelozano <jmestrelozano@gmail.com>
2026-07-28 11:14:36 -0500

    refactor: update dependencies and configuration for improved performance and compatibility

--- src/Store/store.ts
+++ src/Store/store.ts
@@ -1,11 +1,4 @@
-import {
-  AnyAction,
-  configureStore,
-  MiddlewareArray,
-  StoreEnhancer,
-  ThunkMiddleware,
-} from "@reduxjs/toolkit";
-import { CurriedGetDefaultMiddleware } from "@reduxjs/toolkit/dist/getDefaultMiddleware";
+import { configureStore } from "@reduxjs/toolkit";
 import { ICountrySlice, IWeatherSlice } from "../Interfaces/interfaceSlices";
 import { countrySlices } from "./Slices/countrySlices";
 import { weatherSlices } from "./Slices/weatherSlices";
@@ -14,21 +7,18 @@ export interface storeInterface {
   country: ICountrySlice;
   weather: IWeatherSlice;
 }
-export const store = configureStore<
-  storeInterface,
-  AnyAction,
-  MiddlewareArray<[ThunkMiddleware<any, AnyAction, undefined>]>,
-  [StoreEnhancer<{}, {}>]
->({
-  reducer: { country: countrySlices.reducer, weather: weatherSlices.reducer },
-  middleware: (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
+
+export const store = configureStore({
+  reducer: {
+    country: countrySlices.reducer,
+    weather: weatherSlices.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       immutableCheck: false,
       serializableCheck: false,
     }),
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,17 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: [
-            "react",
-            "react-dom",
-            "luxon",
-            "@iconscout/react-unicons",
-            "react-toastify",
-            "@reduxjs/toolkit",
-          ],
+        manualChunks(id) {
+          if (
+            id.includes("node_modules/react") ||
+            id.includes("node_modules/react-dom") ||
+            id.includes("node_modules/luxon") ||
+            id.includes("node_modules/@iconscout/react-unicons") ||
+            id.includes("node_modules/react-toastify") ||
+            id.includes("node_modules/@reduxjs/toolkit")
+          ) {
+            return "vendor";
+          }
         },
       },
     },
